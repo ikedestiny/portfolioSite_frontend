f29b54ac5896c5c616b82b322fabd0f932f8b6d5
Ike Holy Destiny <121898018+ikedestiny@users.noreply.github.com>
2025-01-08 17:58:46 +0300

    cors

--- src/ApiCalls.js
+++ src/ApiCalls.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = "http://127.0.0.1:8080/portfolioSiteBackend-1.0-SNAPSHOT/api";
+const url = "https://portfoliosite-backend-springboot.onrender.com/api";
 
 export const get_all_projects = async () => {
     try {
